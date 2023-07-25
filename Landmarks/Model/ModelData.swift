b69b618debc4f08be84e058dfeccc96dee62451e
Russel Corbo <russelcorbo@gmail.com>
2023-07-25 19:47:56 -0400

    Composing Complex Interfaces - https://developer.apple.com/tutorials/swiftui/composing-complex-interfaces

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -13,6 +13,17 @@ final class ModelData: ObservableObject {
     // An observable obj needs to 'publish' any changes to its data so that
     // it's subscribers can pick up the changes
     var hikes: [Hike] = load("hikeData.json")
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+            )
+    }
 }
 
 
